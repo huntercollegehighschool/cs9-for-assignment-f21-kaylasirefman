fb0b305531ff917cd10d806007846f0fd52dd227
Kayla Sirefman <94643809+kaylasirefman@users.noreply.github.com>
2022-01-06 02:52:31 +0000

    save

--- part4.py
+++ part4.py
@@ -40,3 +40,14 @@ fizz
 """
 
 # write your code here 
+n = int(input("Enter an integer:"))
+
+for i in range(1, n+1)
+  if i % 3 == 0 and i % 5 ==0 
+    print ("fizzbuzz")
+elif i % 5 == 0:
+  print ("buzz")
+elif i % 3 == 0
+  print("fizz")
+else:
+  print i
